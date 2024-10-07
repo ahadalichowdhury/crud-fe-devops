2fdc0822b58b44f40f7ed7a97b0a58420629c423
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2024-10-08 03:11:26 +0600

    added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
 	return (
 		<div className="container">
-			<h1>MERN Stack CRUD</h1>
+			<h1>MERN Stack CRUD with DevOps Pipeline</h1>
 			<p>
 				<b>Front-end</b>: React.js v17+ with RRDv6+
 			</p>
